418d69daf94925f7047bfa0bcfab7fe37c08f4ab
William Marques <junior.stuart34@gmail.com>
2022-03-15 11:29:01 -0400

    fix component Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,11 +8,12 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/WillJR183/ds-movie"></a>
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className="ds-movie-contact-link">/devsuperior</p>
-                    </div>
+                    <a href="https://github.com/WillJR183/ds-movie">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="ds-movie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
         </header>
